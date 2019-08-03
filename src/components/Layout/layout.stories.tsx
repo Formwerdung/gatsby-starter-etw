2e7a5abf43f343b4fb3124abc64eddeedcccae55
Marin Aeschbach <marin.aeschbach@gmail.ch>
2019-08-03 14:16:03 +0200

    Switch linting to eslint

--- src/components/Layout/layout.stories.tsx
+++ src/components/Layout/layout.stories.tsx
@@ -1,11 +1,7 @@
-import { storiesOf } from '@storybook/react';
-import * as React from 'react';
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
 import Layout from './index'
 
-storiesOf("Button", module)
-  .add("with text", () => (
-    <Layout>Hello Button</Layout>
-  ))
-  .add("with some emoji", () => (
-    <Layout>😀 😎 👍 💯</Layout>
-  ));
+storiesOf('Button', module)
+  .add('with text', () => <Layout>Hello Button</Layout>)
+  .add('with some emoji', () => <Layout>😀 😎 👍 💯</Layout>)
