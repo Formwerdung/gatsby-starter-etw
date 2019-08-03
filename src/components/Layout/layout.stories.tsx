8aeff8cf9dcebb4404b4fcb1352617aeceb4c2ce
Marin Aeschbach <marin.aeschbach@gmail.ch>
2019-08-03 14:57:14 +0200

    Rename example story

--- src/components/Layout/layout.stories.tsx
+++ src/components/Layout/layout.stories.tsx
@@ -2,6 +2,6 @@ import { storiesOf } from '@storybook/react'
 import * as React from 'react'
 import Layout from './index'
 
-storiesOf('Button', module)
-  .add('with text', () => <Layout>Hello Button</Layout>)
+storiesOf('Layout', module)
+  .add('with text', () => <Layout>Hello World</Layout>)
   .add('with some emoji', () => <Layout>😀 😎 👍 💯</Layout>)
